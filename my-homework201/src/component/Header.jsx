7f1fcae1cf19843fc8c7bffba6483eadf378c0be
Shoma Takami <hawklook.2331@gmail.com>
2019-08-07 01:20:35 -0700

    2nd commit

---
+++ my-homework201/src/component/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <form>
+      <input type='text' placeholder='Enter todo!' />
+      <input type='submit' value='Add to list' />
+    </form>
+  );
+};
+
+export default Header;
---
+++ my-homework201/src/component/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      Show: <input type='submit' value='All' />
+      <input type='submit' value='Active' />
+      <input type='submit' value='Complited' />
+    </div>
+  );
+};
+
+export default Footer;
